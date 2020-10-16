355442be8532dc95ee6cdf0d67fd3e932d6e18df
German Mora <german.mora.664@gmail.com>
2020-10-16 12:08:02 -0300

    tablero computadora

--- index.js
+++ index.js
@@ -1,67 +1,139 @@
-const board = document.getElementById('board')
-const matriz = [[]];
+const board1 = document.getElementById('board')
+const board2 = document.getElementById('board2')
+const matriz = [];
 const barco1 = [];
 const barco2 = [];
 const barco3 = [];
 const barco4 = [];
-const botonPosiciones = document.getElementById('posiciones')
-let counter = 0;
-const mensaje = document.getElementById("mensaje");
-const disparar = document.getElementById("disparar");
+const barco5 = [];
+const barco6 = [];
+const barco7 = [];
+const botonPosiciones1 = document.getElementById('posiciones1')
+const botonPosiciones2 = document.getElementById('posiciones2')
+let counter1 = 0;
+let counter2 = 0;
+let currentPlayer = 1;
+const mensaje1 = document.getElementById("mensaje1");
+const mensaje2 = document.getElementById("mensaje2");
+const disparar1 = document.getElementById("disparar");
+let numberComputer = 90
 
-const checkIfFind = (e) => {
-    const targetId = parseInt(e.target.id)
-    if (barco1.includes(targetId) || barco2.includes(targetId) || barco3.includes(targetId) || barco4.includes(targetId)) {
-        e.target.classList.add('found')
-        counter++;
-        if (counter === 9) {
-            mensaje.classList.remove('hide')
 
+const computerTurn = () => {
+    if (currentPlayer === 2) {
+        let randomNumber = Math.floor(Math.random() * numberComputer)
+        let intento = matriz[randomNumber]
+        matriz.splice(randomNumber, 1)
+        const target = document.getElementById("board2")
+        const targeted = target.getElementsByClassName(intento)[0]
+        if (barco5.includes(intento) || barco6.includes(intento) || barco7.includes(intento)) {
+
+            targeted.classList.add('found');
+
+            counter2++
+            if (counter2++ === 9) {
+                elmensaje.classList.remove('hide')
+            }
+        } else {
+            targeted.classList.add('missed')
         }
-    } else {
-        e.target.classList.add('missed')
-
+        numberComputer--
+        currentPlayer = 1
+        console.log(matriz)
     }
 }
 
+const checkIfFind = (e, firstBarco, secondBarco, thirdBarco, contador, num) => {
+    if (currentPlayer === num) {
 
-const createBoard = () => {
+        const targetId = parseInt(e.target.id)
+        let realContador;
+        let elmensaje;
+        if (num === 1) {
+            elmensaje = mensaje1
+
+        } else if (num === 2) {
+            elmensaje = mensaje2
+        }
+        if (firstBarco.includes(targetId) || secondBarco.includes(targetId) || thirdBarco.includes(targetId)) {
+            e.target.classList.add('found');
+            if (num === 1) {
+                realContador = counter1++
+
+            } else if (num === 2) {
+                realContador = counter2++
+            }
+            realContador++
+            if (realContador === 9) {
+                elmensaje.classList.remove('hide')
+            }
+        } else {
+            e.target.classList.add('missed')
+        }
+        if (num === 1) {
+            currentPlayer = 2
+
+        } else if (num === 2) {
+            currentPlayer = 1
+        }
+    }
+    computerTurn(num)
+}
+
+
+const createBoard = (board, firstBarco, secondBarco, thirdBarco, num, contador) => {
     for (let i = 0; i <= 89; i++) {
         let box = document.createElement('div')
         box.id = i
-        box.classList.add('boxes')
+        box.classList.add('boxes' + num)
+        box.classList.add(i)
         board.appendChild(box)
-        matriz.push(i)
-        box.addEventListener("click", checkIfFind)
-        if (barco1.length < 1) {
-            randomNumber = Math.floor(Math.random() * 80) + 1
-            if (!barco1.includes(randomNumber)) {
+        if (num === 1) {
+            matriz.push(i)
+        }
+
+        box.addEventListener("click", (e) => {
+            checkIfFind(e, firstBarco, secondBarco, thirdBarco, contador, num)
+        })
+        let randomNumber1 = Math.floor(Math.random() * 89)
+        let randomNumber2 = Math.floor(Math.random() * 89)
+        do {
+            randomNumber2 = Math.floor(Math.random() * 89)
+        } while (randomNumber1 === randomNumber2)
+
+        let randomNumber
+        if (num === 1) {
+            randomNumber = randomNumber2
+        } else {
+            randomNumber = randomNumber1
+        }
+        if (firstBarco.length < 1) {
+
+            if (!firstBarco.includes(randomNumber)) {
                 if ((randomNumber + 1).toString().split('')[1] == 0) {
-                    barco1.push(randomNumber, randomNumber - 1)
+                    firstBarco.push(randomNumber, randomNumber - 1)
                 } else {
-                    barco1.push(randomNumber, randomNumber + 1)
+                    firstBarco.push(randomNumber, randomNumber + 1)
                 }
             }
         }
 
-        if (barco2.length < 1) {
-            randomNumber = Math.floor(Math.random() * 89) + 1
-            if (!barco1.includes(randomNumber) && !barco1.includes(randomNumber - 1) && !barco1.includes(randomNumber - 2) && !barco1.includes(randomNumber + 1) && !barco1.includes(randomNumber + 2) && !barco2.includes(randomNumber)) {
+        if (secondBarco.length < 1) {
+            if (!firstBarco.includes(randomNumber) && !firstBarco.includes(randomNumber - 1) && !firstBarco.includes(randomNumber - 2) && !firstBarco.includes(randomNumber + 1) && !firstBarco.includes(randomNumber + 2) && !secondBarco.includes(randomNumber)) {
                 if ((randomNumber + 1).toString().split('')[1] == 0 || (randomNumber + 2).toString().split('')[1] == 0) {
-                    barco2.push(randomNumber, randomNumber - 1, randomNumber - 2)
+                    secondBarco.push(randomNumber, randomNumber - 1, randomNumber - 2)
                 } else {
-                    barco2.push(randomNumber, randomNumber + 1, randomNumber + 2)
+                    secondBarco.push(randomNumber, randomNumber + 1, randomNumber + 2)
                 }
             }
         }
 
-        if (barco4.length < 1) {
-            randomNumber = Math.floor(Math.random() * 80) + 1;
-            if (!barco1.includes(randomNumber) && !barco1.includes(randomNumber + 10) && !barco1.includes(randomNumber + 20) && !barco1.includes(randomNumber + 30) && !barco1.includes(randomNumber - 10) && !barco1.includes(randomNumber - 20) && !barco1.includes(randomNumber - 30) && !barco2.includes(randomNumber) && !barco2.includes(randomNumber + 10) && !barco2.includes(randomNumber + 20) && !barco2.includes(randomNumber + 30) && !barco2.includes(randomNumber - 10) && !barco2.includes(randomNumber - 20) && !barco2.includes(randomNumber - 30) && !barco3.includes(randomNumber) && !barco3.includes(randomNumber + 10) && !barco3.includes(randomNumber + 20) && !barco3.includes(randomNumber + 30) && !barco3.includes(randomNumber - 10) && !barco3.includes(randomNumber - 20) && !barco3.includes(randomNumber - 30) && !barco4.includes(randomNumber)) {
+        if (thirdBarco.length < 1) {
+            if (!firstBarco.includes(randomNumber) && !firstBarco.includes(randomNumber + 10) && !firstBarco.includes(randomNumber + 20) && !firstBarco.includes(randomNumber + 30) && !firstBarco.includes(randomNumber - 10) && !firstBarco.includes(randomNumber - 20) && !firstBarco.includes(randomNumber - 30) && !secondBarco.includes(randomNumber) && !secondBarco.includes(randomNumber + 10) && !secondBarco.includes(randomNumber + 20) && !secondBarco.includes(randomNumber + 30) && !secondBarco.includes(randomNumber - 10) && !secondBarco.includes(randomNumber - 20) && !secondBarco.includes(randomNumber - 30) && !barco3.includes(randomNumber) && !barco3.includes(randomNumber + 10) && !barco3.includes(randomNumber + 20) && !barco3.includes(randomNumber + 30) && !barco3.includes(randomNumber - 10) && !barco3.includes(randomNumber - 20) && !barco3.includes(randomNumber - 30) && !thirdBarco.includes(randomNumber)) {
                 if (randomNumber > 59) {
-                    barco4.push(randomNumber, randomNumber - 10, randomNumber - 20, randomNumber - 30)
+                    thirdBarco.push(randomNumber, randomNumber - 10, randomNumber - 20, randomNumber - 30)
                 } else {
-                    barco4.push(randomNumber, randomNumber + 10, randomNumber + 20, randomNumber + 30)
+                    thirdBarco.push(randomNumber, randomNumber + 10, randomNumber + 20, randomNumber + 30)
                 }
             }
         }
@@ -71,28 +143,62 @@ const createBoard = () => {
     }
 }
 
-window.addEventListener("load", createBoard)
-disparar.addEventListener("click", () => {
+window.addEventListener("load", () => {
+    createBoard(board1, barco1, barco2, barco4, 1, counter1)
+    createBoard(board2, barco5, barco6, barco7, 2, counter2)
+})
+disparar1.addEventListener("click", () => {
     const valor = parseInt(prompt("Elige el número al que quieres disparar. Ej: 3 Horizontal + 4 Vertical = 34"))
+    let realCounter;
     const boxEscogida = document.getElementById(valor)
     if (barco1.includes(valor) || barco2.includes(valor) || barco4.includes(valor)) {
+        if (currentPlayer === 1) {
+            realContador = counter1++
+
+        } else if (currentPlayer === 2) {
+            realContador = counter2++
+        }
+
+
         boxEscogida.classList.add('found')
-        counter++;
-        if (counter === 9) {
-            mensaje.classList.remove('hide')
+        realContador++;
+        currentPlayer = 2
+        computerTurn()
+        if (realContador === 9) {
+            mensaje1.classList.remove('hide')
         }
     } else {
         boxEscogida.classList.add('missed')
+        currentPlayer = 2
+        computerTurn()
     }
+
 })
 
 
-const boxes = document.getElementsByClassName("boxes")
 
-botonPosiciones.addEventListener("click", () => {
 
-    Array.from(boxes).map(box => {
+const boxes1 = document.getElementsByClassName("boxes1")
+const boxes2 = document.getElementsByClassName("boxes2")
+
+botonPosiciones1.addEventListener("click", () => {
+
+    Array.from(boxes1).map(box => {
         if (barco1.includes(parseInt(box.id)) || barco2.includes(parseInt(box.id)) || barco4.includes(parseInt(box.id))) {
+            if (box.classList.contains("border")) {
+                box.classList.remove('border')
+            } else {
+                box.classList.add('border')
+            }
+
+        }
+    })
+})
+
+botonPosiciones2.addEventListener("click", () => {
+
+    Array.from(boxes2).map(box => {
+        if (barco5.includes(parseInt(box.id)) || barco6.includes(parseInt(box.id)) || barco7.includes(parseInt(box.id))) {
             if (box.classList.contains("border")) {
                 box.classList.remove('border')
             } else {
